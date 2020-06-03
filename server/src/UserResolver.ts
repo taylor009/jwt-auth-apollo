fae36bec6bd5aa461bd0aae2f4bda954830fd567
taylor <taylor@affair.dev>
2020-06-03 09:56:09 -0400

    created createUser mutation and query user mutation also added nodemon dev dependence

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,9 +1,37 @@
-import {Query, Resolver} from 'type-graphql';
+import {Query, Resolver, Mutation, Arg} from 'type-graphql';
+import { hash } from 'bcryptjs';
+import {User} from "./entity/User";
 
 @Resolver()
 export class UserResolver {
     @Query(() => String)
     hello(){
         return 'Hello'
+    }
+
+    @Query(() => [User])
+    users(){
+        return User.find();
+    }
+
+    @Mutation(() => Boolean)
+    async register(
+        @Arg('email') email: string,
+        @Arg('password') password: string
+    ) {
+
+        const hashedPassword = await hash(password, 12);
+
+        try {
+            await User.insert({
+                email,
+                password: hashedPassword
+            });
+        }
+        catch(error) {
+            console.log(error);
+            return false;
+        }
+        return true;
     }
 }
